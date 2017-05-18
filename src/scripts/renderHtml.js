6ea2553945c9cc81228670bf09d2591244921d2a
Jonathan Kempf <jkempf@advance.net>
2017-05-18 16:53:45 -0400

    Starting to bootstrap the HTML templating

---
+++ src/scripts/renderHtml.js
@@ -0,0 +1,17 @@
+const RenderHtml = function () {
+	const renderHtml = {
+		parseObjects: richStoryObj => {
+			// Loop through the story objects
+		},
+		createNodes: objectIndex => {
+			// Setup the DOM nodes to attach objects to 
+		},
+		returnHtml: domNodes => {
+			// Parse out the full HTML string
+		}
+	};
+	return renderHtml;
+};
+
+const render = new RenderHtml();
+export default render;
